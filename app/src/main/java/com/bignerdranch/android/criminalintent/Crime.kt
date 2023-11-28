09c6383c6f9a05a434eace4a46ce671d190a496b
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-11-28 14:07:08 +0700

    Реализация репозитория

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -2,8 +2,8 @@ package com.bignerdranch.android.criminalintent
 
 import java.util.Date
 import java.util.UUID
-
-data class Crime (val id: UUID = UUID.randomUUID(),
+@Entity
+data class Crime (@PrimaryKey val id: UUID = UUID.randomUUID(),
                   var title: String = "",
                   var date: Date = Date(),
                   var isSolved: Boolean = false)
