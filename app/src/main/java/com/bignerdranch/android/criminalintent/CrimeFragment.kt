d423dd3893e31689778ad23b4d67903ff8da055e
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-09 18:43:17 +0700

    Создание экземпляра DialogFragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -17,6 +17,7 @@ import java.util.UUID
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
 class CrimeFragment : Fragment() {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -42,10 +43,6 @@ class CrimeFragment : Fragment() {
         titleField = view.findViewById(R.id.crime_title) as EditText
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
         return view
     }
 
@@ -88,6 +85,16 @@ class CrimeFragment : Fragment() {
                 crime.isSolved = isChecked
             }
         }
+            dateButton.setOnClickListener {
+                DatePickerFragment().apply {
+                    show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+                }
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
     }
 
     private fun updateUI() {
