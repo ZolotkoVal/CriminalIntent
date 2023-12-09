331db745e828f279294321e469de957ef3f58c10
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-09 11:15:32 +0700

    Использование исполнителя

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import database.CrimeDatabase
 import java.util.UUID
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 class CrimeRepository private constructor(context: Context) {
@@ -14,9 +15,21 @@ class CrimeRepository private constructor(context: Context) {
         DATABASE_NAME
     ).build()
     private val crimeDao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()
+
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
-    companion object {
+    fun updateCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.updateCrime(crime)
+        }
+    }
+    fun addCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.addCrime(crime)
+        }
+    }
+        companion object {
         private var INSTANCE: CrimeRepository? = null
         fun initialize(context: Context) {
             if (INSTANCE == null) {
